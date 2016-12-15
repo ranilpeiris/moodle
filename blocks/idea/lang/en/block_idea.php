0841f021f7b902f972c53ec7554c84155ec5c5b5
Colombage Ranil Peiris <crpeiris@gmail.com>
2016-12-15 22:25:01 +0100

    added idea block success

---
+++ blocks/idea/lang/en/block_idea.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Idea';
+$string['idea'] = 'Idea';
+$string['idea:addinstance'] = 'Add a new idea block';
+$string['idea:myaddinstance'] = 'Add a new Idea block to the My Moodle page';
