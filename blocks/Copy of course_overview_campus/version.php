abc856222001b9195a06306211ec16f0192abc51
Colombage Ranil Peiris <crpeiris@gmail.com>
2016-12-18 21:33:22 +0100

    Added my course overview tab

---
+++ blocks/Copy of course_overview_campus/version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block "course overview (campus)" - Version file
+ *
+ * @package    block_course_overview_campus
+ * @copyright  2013 Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_course_overview_campus';
+$plugin->version = 2016110700;
+$plugin->release = 'v3.1-r2';
+$plugin->requires = 2016052300;
+$plugin->maturity = MATURITY_STABLE;
